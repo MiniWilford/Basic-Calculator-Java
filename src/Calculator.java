6a8191b1fb650e492d463b2746851f12f585d5ff
MiniWilford <dejarnettkyle@yahoo.com>
2022-05-27 20:04:23 -0400

    Creating README and base Java file

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    //Simple program reiterating basics of Java syntax
+    public static void main(String[] args) {
+        //Initilize Program Start
+        System.out.println("Hello and welcome to the Java Calculator program.");
+
+    }
+    
+}
